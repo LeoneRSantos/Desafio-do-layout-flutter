3254051d648b357871d85c7690067fd08ddec5de
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-05-27 18:13:22 -0300

    Alterações na terceira linha

--- desafio_do_layout/lib/widgets/terceira_linha.dart
+++ desafio_do_layout/lib/widgets/terceira_linha.dart
@@ -14,7 +14,7 @@ class TerceiraLinha extends StatelessWidget {
               
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.grey[300]),
       height: 140,
-      margin: const EdgeInsets.symmetric(horizontal: 15.0),
+      margin: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         
@@ -25,24 +25,25 @@ class TerceiraLinha extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           
           children: [
-            Text(num1, textAlign: TextAlign.right,),
+            
             Padding(
               padding: const EdgeInsets.only(left: 7.0, right: 7.0),
               child: Card(
                 elevation: 0,
                 color: Colors.grey[300],
                 child: SizedBox(
-                  width: 90,
-                  height: 90,
+                  width: 110,
+                  height: 110,
 
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
 
-                    children: const[
+                    children: [
+                      Text(num1,),
                       ListTile(
-                        title: Icon(Icons.shop_2, size: 40.0,),
-                        subtitle: Text("novos pedidos", textAlign: TextAlign.center,)
+                        title: Icon(Icons.shop_2, size: 40.0, color: Colors.deepPurple,),
+                        subtitle: Text("novos pedidos", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),)
                       ),
                     ],
                   )
@@ -59,7 +60,7 @@ class TerceiraLinha extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(num2),
+            
             Padding(
               padding: const EdgeInsets.only(left: 7.0, right: 7.0),
               
@@ -67,12 +68,13 @@ class TerceiraLinha extends StatelessWidget {
               Card(
                 elevation: 0,
                 color: Colors.grey[300],
-                child: SizedBox(width: 90, height: 90, 
+                child: SizedBox(width: 110, height: 110, 
                 child: Column(
-                  children: const[
+                  children: [
+                    Text(num2),
                     ListTile(
-                      title: Icon(Icons.people, size: 40.0,),
-                      subtitle: Text("novos clientes", textAlign: TextAlign.center,),
+                      title: Icon(Icons.people, size: 40.0, color: Colors.deepPurple,),
+                      subtitle: Text("novos clientes", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),),
                     )
                   ],
                 ),),
@@ -87,16 +89,22 @@ class TerceiraLinha extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(num3),
+            
             Padding(padding: const EdgeInsets.only(left: 7.0, right: 7.0),
             child: Card(
               elevation: 0,
               color: Colors.grey[300],
-              child: const SizedBox(width: 90, height: 90,
-              child: ListTile(
-                title: Icon(Icons.location_city_sharp, size: 40.0,),
-                subtitle: Text("novas cidades", textAlign: TextAlign.center,),
-              ),),
+              child: Column(
+                
+                children: [
+                  Text(num3),
+                  const SizedBox(width: 90, height: 90,
+                  child: ListTile(
+                    title: Icon(Icons.location_city_sharp, size: 40.0, color: Colors.deepPurple,),
+                    subtitle: Text("novas cidades", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),),
+                  ),),
+                ],
+              ),
             ),
             ),
           ],
