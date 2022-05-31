102022aeadcdfd7c22dc355021b9b7ab91e96884
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-05-31 00:03:05 -0300

    Alterações na terceira linha

--- desafio_do_layout/lib/widgets/terceira_linha.dart
+++ desafio_do_layout/lib/widgets/terceira_linha.dart
@@ -14,7 +14,7 @@ class TerceiraLinha extends StatelessWidget {
               
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.grey[300]),
       height: 140,
-      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      margin: const EdgeInsets.symmetric(horizontal: 18.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         
@@ -27,11 +27,8 @@ class TerceiraLinha extends StatelessWidget {
           children: [
             
             Padding(
-              padding: const EdgeInsets.only(left: 7.0, right: 7.0),
-              child: Card(
-                elevation: 0,
-                color: Colors.grey[300],
-                child: SizedBox(
+              padding: const EdgeInsets.all(3.0),
+              child:  SizedBox(
                   width: 110,
                   height: 110,
 
@@ -42,13 +39,12 @@ class TerceiraLinha extends StatelessWidget {
                     children: [
                       Text(num1,),
                       ListTile(
-                        title: Icon(Icons.shop_2, size: 40.0, color: Colors.deepPurple,),
+                        title: Icon(Icons.shop_2, size: 45.0, color: Colors.deepPurple,),
                         subtitle: Text("novos pedidos", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),)
                       ),
                     ],
                   )
                 ),
-              ),
                 
             ),
           ],
@@ -62,23 +58,18 @@ class TerceiraLinha extends StatelessWidget {
           children: [
             
             Padding(
-              padding: const EdgeInsets.only(left: 7.0, right: 7.0),
+              padding: const EdgeInsets.all(3.0),
               
-              child: 
-              Card(
-                elevation: 0,
-                color: Colors.grey[300],
-                child: SizedBox(width: 110, height: 110, 
+              child: SizedBox(width: 110, height: 110, 
                 child: Column(
                   children: [
                     Text(num2),
                     ListTile(
-                      title: Icon(Icons.people, size: 40.0, color: Colors.deepPurple,),
+                      title: Icon(Icons.people, size: 45.0, color: Colors.deepPurple,),
                       subtitle: Text("novos clientes", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),),
                     )
                   ],
                 ),),
-              ),
               
             ),
           ],
@@ -90,28 +81,25 @@ class TerceiraLinha extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             
-            Padding(padding: const EdgeInsets.only(left: 7.0, right: 7.0),
-            child: Card(
-              elevation: 0,
-              color: Colors.grey[300],
-              child: Column(
+            Padding(padding: const EdgeInsets.all(3.0),
+            child: Column(
                 
                 children: [
                   Text(num3),
                   const SizedBox(width: 90, height: 90,
                   child: ListTile(
-                    title: Icon(Icons.location_city_sharp, size: 40.0, color: Colors.deepPurple,),
+                    title: Icon(Icons.location_city_sharp, size: 45.0, color: Colors.deepPurple,),
                     subtitle: Text("novas cidades", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),),
                   ),),
                 ],
               ),
             ),
-            ),
           ],
         ),
 
-          ],
-        ),
+          
+      ]  
+      ),
     );
 }
 }
