f8490e75ec0338c1ffa1c2e64d175548969c7fff
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-20 12:39:35 -0300

    Padronização dos elementos da 3ª linha

--- desafio_do_layout/lib/widgets/terceira_linha.dart
+++ desafio_do_layout/lib/widgets/terceira_linha.dart
@@ -1,105 +1,51 @@
 import 'package:flutter/material.dart';
 
 class TerceiraLinha extends StatelessWidget {
-  const TerceiraLinha({Key? key}) : super(key: key);
+  const TerceiraLinha({Key? key, required this.icone, required this.texto}) : super(key: key);
 
   final String num1 = "12";
   final String num2 = "20";
   final String num3 = "20";
+
+  final IconData icone;
+  final String texto;
   
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-              
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.grey[300]),
-      height: 140,
-      margin: const EdgeInsets.symmetric(horizontal: 18.0),
-      child: Row(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      
+      children: [
+        
+      // Primeiro card
+      Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
         
         children: [
           
-        // Primeiro card
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          
-          children: [
-            
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child:  SizedBox(
-                  width: 110,
-                  height: 110,
-
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
-
-                    children: [
-                      Text(num1,),
-                      ListTile(
-                        title: Icon(Icons.shop_2, size: 45.0, color: Colors.deepPurple,),
-                        subtitle: Text("novos pedidos", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),)
-                      ),
-                    ],
-                  )
-                ),
-                
-            ),
-          ],
-        ),
-
-
-        // Segundo card
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-            Padding(
-              padding: const EdgeInsets.all(3.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(width: 110, height: 110,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
               
-              child: SizedBox(width: 110, height: 110, 
-                child: Column(
-                  children: [
-                    Text(num2),
-                    ListTile(
-                      title: Icon(Icons.people, size: 45.0, color: Colors.deepPurple,),
-                      subtitle: Text("novos clientes", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),),
-                    )
-                  ],
-                ),),
-              
-            ),
-          ],
-        ),
-
-      // Terceiro card
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-            Padding(padding: const EdgeInsets.all(3.0),
-            child: Column(
-                
                 children: [
-                  Text(num3),
-                  const SizedBox(width: 90, height: 90,
-                  child: ListTile(
-                    title: Icon(Icons.location_city_sharp, size: 45.0, color: Colors.deepPurple,),
-                    subtitle: Text("novas cidades", textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),),
-                  ),),
+                  Text(num1,),
+                  ListTile(
+                    title: Icon(icone, size: 45.0, color: Colors.deepPurple,),
+                    subtitle: Text(texto, textAlign: TextAlign.center, style: TextStyle(color: Colors.purple),)
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-
-          
-      ]  
+          ),
+        ],
       ),
+  
+    ]  
     );
 }
 }
