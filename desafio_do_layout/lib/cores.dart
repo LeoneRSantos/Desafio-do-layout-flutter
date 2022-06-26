72d0b17a672c105e9631238e73dd12488cbfd780
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-26 13:13:19 -0300

    Criação da classe Cores

---
+++ desafio_do_layout/lib/cores.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Cores {
+  final Color _roxoPrincipal = Color(0xFF262457);
+  final Color _roxoSecundario = Colors.purple;
+  final Color _roxoTerceario = Color(0xFF4f426f);
+
+  Color obterPrincipal(){
+
+    return _roxoPrincipal;
+  }
+
+  Color obterSecundario(){
+    
+    return _roxoSecundario;
+  }
+
+  Color obterTerceario(){
+    
+    return _roxoTerceario;
+  }
+
+}
