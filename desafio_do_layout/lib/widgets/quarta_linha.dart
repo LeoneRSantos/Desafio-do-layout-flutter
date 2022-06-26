dbec302e4a14e2fc663532214b339a79a09c986d
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-26 13:14:56 -0300

    Refatoração na Quarta Linha

--- desafio_do_layout/lib/widgets/quarta_linha.dart
+++ desafio_do_layout/lib/widgets/quarta_linha.dart
@@ -1,25 +1,32 @@
 import 'dart:ui';
 
+import 'package:desafio_do_layout/cores.dart';
+import 'package:desafio_do_layout/widgets/textos.dart';
 import 'package:flutter/material.dart';
 
 class QuartaLinha extends StatelessWidget {
-  const QuartaLinha({Key? key}) : super(key: key);
+  QuartaLinha({Key? key}) : super(key: key);
 
   final String valor = "34.000,00";
+  late Cores c = Cores();
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Icon(Icons.shop_2, size: 55.0,),
+        Icon(Icons.shop_2, size: 55.0, color: c.obterPrincipal(),),
 
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text("R\$ $valor", style: TextStyle(fontSize: 35.0),),
-            Text("em novos pedidos", textAlign: TextAlign.right, style: TextStyle(fontSize: 20.0),),
+            Text("R\$ $valor", style: TextStyle(fontSize: 35.0, color: c.obterSecundario(), fontFamily: 'Concert One'),),
+            // Text("em novos pedidos", textAlign: TextAlign.right, style: TextStyle(fontSize: 20.0),),
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Textos(texto: 'em novos pedidos', tamanho: 20.0, cor: c.obterPrincipal()),
+            ),
           ],
         )
       ],
