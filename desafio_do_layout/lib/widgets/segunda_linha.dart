3faa3e4b381fc9b2e64e4469b78592e8ba198b1a
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-27 23:00:12 -0300

    Conversão da SegundaLinha

--- desafio_do_layout/lib/widgets/segunda_linha.dart
+++ desafio_do_layout/lib/widgets/segunda_linha.dart
@@ -2,21 +2,28 @@ import 'package:desafio_do_layout/cores.dart';
 import 'package:desafio_do_layout/widgets/textos.dart';
 import 'package:flutter/material.dart';
 
-class SegundaLinha extends StatelessWidget {
-  SegundaLinha({Key? key}) : super(key: key);
+class SegundaLinha extends StatefulWidget {
+  SegundaLinha({Key? key, required this.alterarValores,}) : super(key: key);
 
+  late void Function() alterarValores;
+
+  @override
+  State<SegundaLinha> createState() => _SegundaLinhaState();
+}
+
+class _SegundaLinhaState extends State<SegundaLinha> {
  late Cores c =Cores();
 
   @override
   Widget build(BuildContext context) {
     return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-              children: [
-                Textos(texto: 'Parabéns! Esse mês você fez', tamanho: 16.0, cor: c.obterSecundario()),
+      children: [
+        Textos(texto: 'Parabéns! Esse mês você fez', tamanho: 16.0, cor: c.obterSecundario()),
 
-                Icon(Icons.visibility, size: 30.0, color: c.obterTerceario()),
-              ],
-              );
+        Icon(Icons.visibility, size: 30.0, color: Theme.of(context).primaryColor),
+      ],
+      );
   }
 }
