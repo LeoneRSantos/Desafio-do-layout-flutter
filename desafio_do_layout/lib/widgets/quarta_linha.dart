38a77055d5a61b46eb6813f557b65aa5fd1ab144
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-06-30 10:23:38 -0300

    Refatoração na Quarta Linha

--- desafio_do_layout/lib/widgets/quarta_linha.dart
+++ desafio_do_layout/lib/widgets/quarta_linha.dart
@@ -15,17 +15,17 @@ class QuartaLinha extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Icon(Icons.shop_2, size: 55.0, color: c.obterPrincipal(),),
+        Icon(Icons.shop_2, size: 55.0, color: Theme.of(context).colorScheme.primary,),
 
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text("R\$ $valor", style: TextStyle(fontSize: 35.0, color: c.obterSecundario(), fontFamily: 'Concert One'),),
+            Text("R\$ $valor", style: TextStyle(fontSize: 35.0, color: Theme.of(context).colorScheme.secondary, fontFamily: 'Concert One'),),
             // Text("em novos pedidos", textAlign: TextAlign.right, style: TextStyle(fontSize: 20.0),),
             Padding(
               padding: const EdgeInsets.only(top: 5.0),
-              child: Textos(texto: 'em novos pedidos', tamanho: 20.0, cor: c.obterPrincipal()),
+              child: Textos(texto: 'em novos pedidos', tamanho: 20.0, cor: Theme.of(context).colorScheme.primary),
             ),
           ],
         )
