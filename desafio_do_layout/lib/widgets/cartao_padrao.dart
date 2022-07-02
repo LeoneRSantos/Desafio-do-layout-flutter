da7b99b3e3c7f8f30b78c458a15246e2e793d6d2
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-02 10:37:55 -0300

    Elevação do Cartão Padrão

--- desafio_do_layout/lib/widgets/cartao_padrao.dart
+++ desafio_do_layout/lib/widgets/cartao_padrao.dart
@@ -12,7 +12,17 @@ class CartaoPadrao extends StatelessWidget {
       padding: const EdgeInsets.all(10.0),
       child: Container(  
         margin: EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20.0), color: Theme.of(context).colorScheme.surface,),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0), 
+          color: Theme.of(context).colorScheme.surface, 
+          boxShadow: [ 
+            BoxShadow(
+            color: Colors.grey.shade400,
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+        ),
+        ]),
         child: conteudo,
       ),
     )
